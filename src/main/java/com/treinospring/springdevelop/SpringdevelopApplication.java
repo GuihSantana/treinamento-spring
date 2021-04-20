9fc087ebbc37e8670bd04efe1e5e3d93c6ad1671
Guilherme <santanaguisantos@gmail.com>
2021-04-19 21:31:39 -0300

    Projeto criado

---
+++ src/main/java/com/treinospring/springdevelop/SpringdevelopApplication.java
@@ -0,0 +1,13 @@
+package com.treinospring.springdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdevelopApplication.class, args);
+	}
+
+}
