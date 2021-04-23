3215ecc46655f1d30946893522932b4910e9d732
Guilherme <santanaguisantos@gmail.com>
2021-04-22 23:54:45 -0300

    Join Categoria e Produto

--- src/main/java/com/treinospring/springdevelop/SpringdevelopApplication.java
+++ src/main/java/com/treinospring/springdevelop/SpringdevelopApplication.java
@@ -1,7 +1,9 @@
 package com.treinospring.springdevelop;
 
 import com.treinospring.springdevelop.domain.Categoria;
+import com.treinospring.springdevelop.domain.Produto;
 import com.treinospring.springdevelop.repositories.CategoriaRepository;
+import com.treinospring.springdevelop.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class SpringdevelopApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdevelopApplication.class, args);
 	}
@@ -24,7 +29,21 @@ public class SpringdevelopApplication implements CommandLineRunner {
 		Categoria categoriaInformatica = new Categoria(null,"Informática");
 		Categoria categoriaEscritório = new Categoria(null,"Escritório");
 
+		Produto computador = new Produto(null, "Computador",2000.00);
+		Produto impressora = new Produto(null, "Impressora",1300.00);
+		Produto mouse = new Produto(null, "Mouse",80.00);
+
+		categoriaInformatica.getProdutos().addAll(Arrays.asList(computador,impressora,mouse));
+		categoriaEscritório.getProdutos().add(impressora);
+
+		computador.getCategorias().add(categoriaInformatica);
+		impressora.getCategorias().addAll(Arrays.asList(categoriaEscritório,categoriaInformatica));
+		mouse.getCategorias().add(categoriaInformatica);
+
+
+
 		categoriaRepository.saveAll(Arrays.asList(categoriaInformatica,categoriaEscritório));
+		produtoRepository.saveAll(Arrays.asList(computador,impressora,mouse));
 
 	}
 }
---
+++ src/main/java/com/treinospring/springdevelop/domain/Produto.java
@@ -0,0 +1,86 @@
+package com.treinospring.springdevelop.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+@Entity
+public class Produto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private Double preco;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "PRODUTO_CATEGORIA",
+            joinColumns = @JoinColumn(name = "produto_id"),
+            inverseJoinColumns = @JoinColumn(name = ("categoria_id"))
+    )
+    private List<Categoria> categorias = new ArrayList<>();
+
+    public Produto() {
+    }
+
+    public Produto(Integer id, String nome, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Produto setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Produto setNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public Produto setPreco(Double preco) {
+        this.preco = preco;
+        return this;
+    }
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public Produto setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(id, produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
