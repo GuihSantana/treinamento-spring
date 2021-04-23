b7b56b91831b706beab77e29bba25ea97e5249d0
Guilherme <santanaguisantos@gmail.com>
2021-04-22 23:30:58 -0300

    Criando operação de instanciação

--- src/main/java/com/treinospring/springdevelop/SpringdevelopApplication.java
+++ src/main/java/com/treinospring/springdevelop/SpringdevelopApplication.java
@@ -1,13 +1,30 @@
 package com.treinospring.springdevelop;
 
+import com.treinospring.springdevelop.domain.Categoria;
+import com.treinospring.springdevelop.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringdevelopApplication {
+public class SpringdevelopApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdevelopApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoriaInformatica = new Categoria(null,"Informática");
+		Categoria categoriaEscritório = new Categoria(null,"Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(categoriaInformatica,categoriaEscritório));
+
+	}
 }
